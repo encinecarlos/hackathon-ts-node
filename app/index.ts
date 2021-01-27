fb61a6a1fc0969ca1b976531ffde219cb9f8d710
Carlos Encine <contato@carlosencine.com>
2021-01-26 21:51:58 -0300

    add cors

--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,18 @@
+require("dotenv").config();
 import express from "express";
-// const db = require("./config/db");
+const cors = require("cors");
 
 const consign = require("consign");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const options = {
+  origin: "*",
+  methods: ["GET", "PUT", "POST"],
+};
+
 // app.db = db;
-// app.use(db);
+app.use(cors(options));
 app.use(express.json());
 
 consign().include("./controllers").then("./config/routes.ts").into(app);
